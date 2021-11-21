0698785436bfe285f9571ff0c4067ea0e932b031
goTl <63504873+EgoTl@users.noreply.github.com>
2021-11-21 11:38:52 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,156 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //int x = 8, y = 4, z = 2;
+        //System.out.println(x + y + z);
+
+        String firstName = "Jhon ";
+        String lastName = "Deer";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+//--------------------------------------------------------------------------------
+        float f1 = 14e3f;
+        double d1 = 25E2d;
+        System.out.println(f1);
+        System.out.println(d1);
+//------------------------------------------------------------------------------
+        //boolean isJavaFun = true;
+        //boolean isFishTasty = false;
+        //System.out.println(isJavaFun);
+        //System.out.println(isFishTasty);
+//------------------------------------------------------------------------------
+        //char a = 111, b = 14, c = 55;
+        //System.out.println(a);
+        //System.out.println(b);
+        //System.out.println(c);
+//-------------------------------------------------------------------------------
+        //double myDouble = 9.78d;
+        //int myInt = (int) myDouble;
+        //System.out.println(myDouble);
+        //System.out.println(myInt);
+//--------------------------------------------------------------------------------
+        //double x = 133;
+        //double y = 56;
+        //System.out.println(x / y);
+//-------------------------------------------------------------------------------
+        //String txt = "But I am not there";
+        //System.out.println("The lenght of the text is: " + txt.length());
+//--------------------------------------------------------------------------------
+        //String txt = "Janis gaja pa ielu un vinu nobrauca masina";
+        //System.out.println(txt.indexOf("ielu"));
+//-----------------------------------------------------------------------------
+        //System.out.println(Math.sqrt(583));
+//-----------------------------------------------------------------------------
+        //System.out.println(Math.random());
+//--------------------------------------------------------------------------
+        //int randomNum = (int)(Math. random() * 150000);
+        //System.out.println(randomNum);
+//--------------------------------------------------------------------------
+        //System.out.println(10 > 9); //boolean true/flase
+//--------------------------------------------------------------------------
+        //if (20 > 18) {
+            //System.out.println("20 is greater than 18");
+        //}
+//-------------------------------------------------------------------------
+       //int time = 24;
+       //if (time < 14){
+           //System.out.println("Good morning.");
+       //} else if (time <20) {
+           //System.out.println("Good day.");
+       //} else {
+           //System.out.println("Good evening.");
+       //}
+//-------------------------------------------------------------------------
+        //int time = 16;
+        //String result = (time < 17 ) ? "Good day." : "Good evening.";
+        //System.out.println(result);
+//----------------------------------------------------------------------
+        //int day = 5;
+        //switch (day) {
+            //case 1:
+                //System.out.println("Monday");
+                //break;
+           // case 2:
+               // System.out.println("Tuesday");
+                //break;
+            //case 3:
+                //System.out.println("Wednesday");
+                //break;
+            //case 4:
+               // System.out.println("Thursday");
+                //break;
+            //case 5:
+                //System.out.println("Friday");
+                //break;
+            //case 6:
+                //System.out.println("Saturday");
+                //break;
+            //case 7:
+               // System.out.println("Sunday");
+                //break;
+        //}
+//---------------------------------------------------------------------------
+        //int day = 4;
+        //switch (day) {
+            //case 6:
+                //System.out.println("Today is thursday");
+                //break;
+            //case 7:
+                ///System.out.println("Today is sunday");
+                //break;
+            ///default:
+               //System.out.println("Looking forward to weekend");
+        //}
+//----------------------------------------------------------------------------
+        //int i = 0;
+        //while (i < 5) {
+           //System.out.println(i);
+            //i++;
+        //}
+//-------------------------------------------------------------------------
+        //int i = 0;
+        //do {
+            //System.out.println(i);
+            //i++;
+        //}
+       //while (i < 5);
+//--------------------------------------------------------------------
+        //for (int i = 0; i < 5; i++) {
+            //System.out.println(i);
+        //}
+//--------------------------------------------------------------------
+        //for (int i = 0; i<= 10; i = i + 2){
+            //System.out.println(i);
+        //}
+//-----------------------------------------------------------------------
+        //String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+        //for (String i : cars) {
+            //System.out.println(i);
+        //}
+//-------------------------------------------------------------------
+        //for (int i = 0; i < 10; i++) {
+            //if (i == 4) {
+                //break;
+            //}
+            //System.out.println(i);
+        //}
+//------------------------------------------------------------------
+        //int i = 0;
+        //while (i < 10) {
+            //if (i == 4) {
+                //i++;
+                //continue;
+            //}
+            //System.out.println(i);
+            //i++;
+        //}
+//-----------------------------------------------------------------------
+
+    }
+    //Vairiables, java if statement, for loops, scanner input java
+    }
+
+
